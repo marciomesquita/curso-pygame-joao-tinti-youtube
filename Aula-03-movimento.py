27306a8c5559f4e15fa03e0ea2f21f2bf4344461
marcio mesquita <botasebatatas@gmail.com>
2024-05-04 20:29:58 -0300

    Aula 3 - Movimento

--- Aula-03-movimento.py
+++ Aula-03-movimento.py
@@ -6,17 +6,22 @@ pygame.init()
 
 largura = 640
 altura = 480
+x = 0
+y = 0
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
 
 while True:
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
 
-    pygame.draw.rect(tela, (255,0,0), (200,300,40,50))
-    pygame.draw.circle(tela, (0,0,120), (300, 260), 40)
-    pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5)
+    pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+    if y >= altura:
+        y = 0
+    y += 1
+    
     pygame.display.update()
