ef292c65fb70bc4fd95657a2fb65666a8eca5186
marcio mesquita <botasebatatas@gmail.com>
2024-05-04 20:34:56 -0300

    Aula 03 - Frame rate

--- Aula-03-movimento.py
+++ Aula-03-movimento.py
@@ -11,8 +11,10 @@ y = 0
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
+relogio = pygame.time.Clock()
 
 while True:
+    relogio.tick(10)
     tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -22,6 +24,6 @@ while True:
     pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
     if y >= altura:
         y = 0
-    y += 1
+    y += 5
     
     pygame.display.update()
